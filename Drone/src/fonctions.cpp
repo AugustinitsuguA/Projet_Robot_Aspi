a7b1d0b6f840db80c439fc771fe806272a2957dc
yohann.holler@gmail.com <yohann.holler@gmail.com>
2026-09-15 11:02:03 +0200

    config robot drone

--- Drone/src/fonctions.cpp
+++ Drone/src/fonctions.cpp
@@ -1,23 +1,3 @@
 #include "../include/fonctions.h"
 #include <WiFi.h>
 #include <ESP32Servo.h>
-
-void avancer(servo &servo_gauche, servo &servo_droit, int vitesse) {
-  servo_gauche.write(vitesse);
-  servo_droit.write(vitesse);
-}
-
-void reculer(servo &servo_gauche, servo &servo_droit, int vitesse) {
-  servo_gauche.write(-vitesse);
-  servo_droit.write(-vitesse);
-}
-
-void gauche (servo &servo_gauche, servo &servo_droit, int vitesse) {
-  servo_gauche.write(-vitesse);
-  servo_droit.write(vitesse);
-}
-
-void droite (servo &servo_gauche, servo &servo_droit, int vitesse) {
-  servo_gauche.write(vitesse);
-  servo_droit.write(-vitesse);
-}
